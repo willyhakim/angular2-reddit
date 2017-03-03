7478d5beded890f2a7ce8709ad8ad957a522d4d2
William Hakizimana <willyha24@gmail.com>
2017-03-03 15:21:09 -0600

    Reddit is a go

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { 
+	Component,
+} from '@angular/core';
+
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+	articles: Article[]
+
+	constructor(){
+	this.articles = [
+    new Article('Angular 2','https://angular.io', 10),
+    new Article('Angular 2','https://angular.io', 10),
+    new Article('Angular 2','https://angular.io', 10),
+    new Article('Angular 2','https://angular.io', 10)
+    ]
+ 	}
+  
+  addArticle( title: HTMLInputElement, link: HTMLInputElement): boolean {
+  	console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+  	this.articles.push(new Article(title.value, link.value, 0));
+  	title.value = '';
+  	link.value = '';
+  	return false;
+  }
+
+  sortedArticles(): Article[]{
+  	return this.articles.sort((a: Article, b: Article)=>b.votes - a.votes);
+  }
 }
---
+++ src/app/article/article.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css'],
+  host:{
+  	class: 'row'
+  }
+})
+export class ArticleComponent implements OnInit {
+	@Input() article: Article;
+
+  constructor() { 
+    this.article = new Article(
+      'Angular 2',
+      'https://angular.io',
+      10
+      );
+  }
+
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+    }
+
+  voteDown(): boolean {
+    this.article.voteDown();
+    return false;
+    }
+
+  ngOnInit() {
+  }
+
+}
